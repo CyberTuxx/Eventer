70c2501651d1dc887c1881e594e7f0e4c7e5a785
CyberTuxx <mathis.vinit@gmail.com>
2021-05-07 21:14:06 +0200

    initial commit

---
+++ app/src/main/java/com/example/home/eventer/Accueil.java
@@ -0,0 +1,10 @@
+package com.example.home.eventer;
+
+/**
+ * Created by Home on 26/06/2017.bile java
+ */
+
+public class Accueil{
+
+
+}
---
+++ app/src/main/java/com/example/home/eventer/MapsActivity.java
@@ -0,0 +1,37 @@
+package com.example.home.eventer;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng iss = new LatLng(45.7869118, 4.8814631);
+        mMap.addMarker(new MarkerOptions().position(iss).title("Presentation PTUT"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(iss));
+    }
+}
